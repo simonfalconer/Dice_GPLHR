025ff045c795f6a074c7395bf762dab49894d34c
Simon Falconer <108297011+simonfalconer@users.noreply.github.com>
2025-09-02 12:51:26 -0400

    Update input.h
    Added davidsonShift variable to schedule and public

--- SHCI/input.h
+++ SHCI/input.h
@@ -42,6 +42,7 @@ struct schedule {
     ar & davidsonTol                          \
     & davidsonTolLoose                        \
     & RdmType                                 \
+    & davidsonShift                           \
     & DavidsonType                            \
     & epsilon2                                \
     & epsilon2Large                           \
@@ -116,6 +117,7 @@ struct schedule {
   double davidsonTol;
   double davidsonTolLoose;
   rdmType RdmType;
+  double davidsonShift;
   davidsonType DavidsonType;
   double epsilon2;
   double epsilon2Large;
